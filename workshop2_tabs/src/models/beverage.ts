3c93eee8784f6a17899c2316b8d1e4a9cfe8b49d
Thaweechok1920 <35834033+Thaweechok1920@users.noreply.github.com>
2018-05-04 14:27:58 +0700

    Initial commit

---
+++ workshop2_tabs/src/models/beverage.ts
@@ -0,0 +1,7 @@
+export interface Beverage{
+    menu_code:string;
+    menu_name:string;
+    price:number;
+    img:string;
+
+}
---
+++ workshop2_tabs/src/pages/home/home.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Beverage } from '../../models/beverage';
+import { Subscription } from 'rxjs/Subscription';
+import { BeverageServiceProvider } from '../../providers/beverage-service/beverage-service';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  beverageList: Beverage[];
+  subscription: Subscription;
+
+  constructor(public navCtrl: NavController,private beverageService: BeverageServiceProvider) {
+
+  }
+private getData(){
+  this.subscription = this.beverageService.getAllData().subscribe((beverageList:Beverage[])=> this.beverageList = beverageList);
+}
+
+ionViewWillEnter(){
+  this.getData();
+}
+
+ionViewWillLeave(){
+  this.subscription.unsubscribe();
+}
+
+}
